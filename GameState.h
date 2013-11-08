066800891d2f2f626cb7c536274861ab9c6e9ce3
aaronlockhart <emanuel.moldovan@gmail.com>
2013-11-08 18:03:23 +0200

    Game State stuff

---
+++ GameState.h
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+// Make it abstract cuz we can't know fo' shizzle
+class GameState
+{
+private:
+    std::string desc;
+    bool isPaused;
+  
+public:
+    GameState(std::string);
+    
+    // Init the stat(ment)
+    virtual void Init() = 0;
+    virtual void Cleanup() = 0;
+    
+    // Self explained, mandatory comments
+    virtual void Pause() = 0;
+    virtual void Resume() = 0;
+    
+    // This thingy handles events and the other one updates the game state
+    void Events();
+    void Update();
+    
+    void Draw();
+}
