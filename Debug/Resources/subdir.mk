ccc6ca7d343613b005b06c243b2c416fdcf360a0
Antonis Kalou <kalouantonis@gmail.com>
2013-11-09 17:25:54 +0200

    Makefile for resources

---
+++ Debug/Resources/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../Resources/TextureManager.cpp 
+
+OBJS += \
+./Resources/TextureManager.o 
+
+CPP_DEPS += \
+./Resources/TextureManager.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Resources/%.o: ../Resources/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -D__GXX_EXPERIMENTAL_CXX0X__ -D_DEBUG -I"/home/slacker/projects/Game" -O0 -g3 -Wall -c -fmessage-length=0 -std=c++11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
