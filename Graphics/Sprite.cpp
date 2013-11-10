aa8d8f5ce067cba764722ddc5fba9fe5d5f01b64
Antonis Kalou <kalouantonis@gmail.com>
2013-11-10 16:55:39 +0200

    Added rotation

--- Graphics/Sprite.cpp
+++ Graphics/Sprite.cpp
@@ -8,6 +8,7 @@ Sprite::Sprite(SDL_Renderer* renderer)
 	, m_bVisible(false)
 	, m_pRendTarget(renderer)
 	, m_pTexture(nullptr)
+	, m_dstRect()
 {
 	if(!renderer)
 		throw std::runtime_error("Sprite::Sprite -- Invalid renderer");
@@ -15,33 +16,49 @@ Sprite::Sprite(SDL_Renderer* renderer)
 
 Sprite::~Sprite()
 {
-	Dispose();
+	dispose();
 }
 
-void Sprite::LoadImage(const std::string& filename)
+void Sprite::loadImage(const std::string& filename)
 {
     // Load texture from image
     m_pTexture = IMG_LoadTexture(m_pRendTarget, filename.c_str());
     
     if(!m_pTexture)
     {
-        std::cout << IMG_GetError() << "\n";
         throw std::runtime_error("Failed to load image: " + filename + "\n\t" + IMG_GetError());
     }
 
     m_bVisible = true;
 }
 
-void Sprite::Draw()
+void Sprite::rotate(double rot)
 {
-	if(m_bVisible)
-		SDL_RenderCopy(m_pRendTarget, m_pTexture, NULL, NULL);
+	m_rotation += rot;
+
+	if(m_rotation > 360)
+		m_rotation = 0;
+	else if(m_rotation < 0)
+		m_rotation = 360;
 }
 
-void Sprite::Dispose()
+void Sprite::draw()
+{
+	m_dstRect.h = m_height;
+	m_dstRect.w = m_width;
+	m_dstRect.x = m_position.x;
+	m_dstRect.y = m_position.y;
+
+	if(m_bVisible)
+		SDL_RenderCopyEx(m_pRendTarget, m_pTexture, NULL, &m_dstRect, m_rotation, NULL, SDL_FLIP_NONE);
+
+}
+
+void Sprite::dispose()
 {
 	if(m_pTexture)
 		SDL_DestroyTexture(m_pTexture);
 
 	m_bVisible = false;
 }
+
--- Graphics/Sprite.h
+++ Graphics/Sprite.h
@@ -5,52 +5,45 @@
 #include <SDL2/SDL.h>
 
 #include <string>
-#include <iostream>
 
 #include <Math/vector2.h>
-
-
-enum Collision
-{
-    COLLISION_NONE,
-    COLLISION_RECT,
-    COLLISION_DIST
-};
 
 class Sprite
 {
     private:
         int m_height, m_width;
         Vector2 m_position;
+        double m_rotation;
 
         bool m_bVisible;
         
         SDL_Renderer *m_pRendTarget;
         SDL_Texture *m_pTexture;
 
+        // Store it in here so that we don't have to create a new
+        // one every draw loop
+		SDL_Rect m_dstRect;
+
     public:
         // Constructor with filename and size ( height, width);
         Sprite(SDL_Renderer*);
         ~Sprite();
         
-        void LoadImage(const std::string& filename);
+        void loadImage(const std::string& filename);
         // Draw the sprite with the target window
-        void Draw();
-        void Dispose();
+        void draw();
+        void dispose();
 
         bool isVisible() { return m_bVisible; }
         void setVisible(bool visible) { m_bVisible = visible; }
         
         // Set height or width
-        
         void setHeight(int h) { m_height = h;}
         void setWidth(int w) { m_width = w; }
         
         int getHeight() { return m_height; }
         int setWidth() { return m_width; }
 
-        // Set positon
-        // example: object.posX() = value
         const Vector2& getPosition() { return m_position; }
         float getX() { return m_position.x; }
         float getY() { return m_position.y; }
@@ -58,6 +51,14 @@ class Sprite
         void setX(float x) { m_position.x = x; }
         void setY(float y) { m_position.y = y; }
         void setPosition(const Vector2& pos) { m_position = pos; }
+
+        // Rotation
+        void setRotation(double rot) { m_rotation = rot; }
+        /**
+         * Rotate the sprite by using its previous rotation
+         */
+        void rotate(double rot);
+        double getRotation() { return m_rotation; }
 };
 
 #endif
