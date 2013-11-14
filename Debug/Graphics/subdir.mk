4ed1ef38a2d3b7b8514928e9a593b77bb2307518
Antonis Kalou <slacker@solydxk>
2013-11-14 16:51:08 +0200

    Makefile changes

--- Debug/Graphics/subdir.mk
+++ Debug/Graphics/subdir.mk
@@ -4,14 +4,17 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
+../Graphics/Camera2D.cpp \
 ../Graphics/Color.cpp \
 ../Graphics/Sprite.cpp 
 
 OBJS += \
+./Graphics/Camera2D.o \
 ./Graphics/Color.o \
 ./Graphics/Sprite.o 
 
 CPP_DEPS += \
+./Graphics/Camera2D.d \
 ./Graphics/Color.d \
 ./Graphics/Sprite.d 
 
@@ -20,7 +23,7 @@ CPP_DEPS += \
 Graphics/%.o: ../Graphics/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -D__GXX_EXPERIMENTAL_CXX0X__ -D_DEBUG -I"/home/slacker/projects/Game" -O0 -g3 -Wall -c -fmessage-length=0 -std=c++11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -D_DEBUG -I"/home/slacker/Projects/Game" -O0 -g3 -Wall -c -fmessage-length=0 -std=c++11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
